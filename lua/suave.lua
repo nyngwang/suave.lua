fdde2c2268b3168b310b968fe5289559d1edf15d
nyngwang <nyngwang@gmail.com>
2023-01-04 20:45:05 +0800

    refactor: NIT

--- lua/suave.lua
+++ lua/suave.lua
@@ -3,43 +3,61 @@ vim.api.nvim_create_augroup('suave.lua', { clear = true })
 local M = {}
 local FOLDER_NAME = '.suave'
 
----------------------------------------------------------------------------------------------------
+
 local function get_project_suave_path()
+  -- This implicitly assume that every project should have one fixed `cd`.
   return vim.fn.getcwd(-1, -1) .. '/' .. FOLDER_NAME
 end
 
-
-function M.suave_folder_is_there()
-  -- assume users won't call `cd` when they want to change the path for each new tab created.
-  local project_suave_path = get_project_suave_path()
-  local yes, _, code = os.rename(project_suave_path, project_suave_path)
-  return yes or (code == 13)
-end
 
 local function total_qflists()
   return vim.fn.getqflist({ nr='$' }).nr
 end
 
+
 local function get_the_menu_id()
   for i = 1, total_qflists() do
-    if vim.fn.getqflist({ nr=i, title=0 }).title == FOLDER_NAME
-      then return vim.fn.getqflist({ nr=i, id=0 }).id end
+    if vim.fn.getqflist({ nr=i, title=0 }).title == FOLDER_NAME then
+      return vim.fn.getqflist({ nr=i, id=0 }).id
+    end
   end
-  local z_on_s = vim.fn.setqflist({}, ' ', { nr='$', title=FOLDER_NAME })
-  if z_on_s ~= 0 then
+  local zero_on_success = vim.fn.setqflist({}, ' ', { nr='$', title=FOLDER_NAME })
+  if zero_on_success ~= 0 then
     print('Suave: Failed to create suave menu!')
     return nil
   end
   return get_the_menu_id()
 end
 
+
+local function refresh_the_menu()
+  -- prepare items.
+  local items = {}
+  for dir in io.popen([[ find ]] .. get_project_suave_path() .. [[ -name '*.vim' ]]):lines() do
+    items[#items+1] = {
+      filename = vim.fn.fnamemodify(dir, ':t'),
+      lnum = tonumber(string.sub(io.popen([[ stat -f %Sm -t %Y%m%d%H%M ]] .. dir):read(), 3, 10)), -- timestamp
+      -- TODO: should maintain a mapping file to store users' note on each session.
+      text = '',
+    }
+  end
+  -- populate items.
+  vim.fn.setqflist({}, 'r', {
+    id = get_the_menu_id(),
+    items = items,
+  })
+end
+
+
 local function switch_to_the_menu()
   vim.cmd('chi ' .. vim.fn.getqflist({ id=get_the_menu_id(), nr=0 }).nr)
 end
 
+
 local function the_menu_did_build()
   return get_the_menu_id()
 end
+
 
 local function get_the_menu_winid()
   local id = get_the_menu_id()
@@ -48,15 +66,39 @@ local function get_the_menu_winid()
   return winid > 0 and winid or nil
 end
 
+
 local function the_menu_is_open()
   return get_the_menu_winid()
 end
+
 
 local function cursor_is_at_the_menu()
   if get_the_menu_winid() == vim.api.nvim_get_current_win() then return true end
   return false
 end
 
+
+local function disable_local_qf_highlight()
+  local function _disable_local_qf_highlight()
+    if cursor_is_at_the_menu() then
+      vim.cmd([[
+        hi __SUAVE_QF_DISABLE guibg=NONE guifg=Directory
+        hi __SUAVE_NO_CURSORLINE guibg=NONE guifg=NONE
+      ]])
+      vim.cmd('set winhl=QuickFixLine:__SUAVE_QF_DISABLE,CursorLine:__SUAVE_NO_CURSORLINE')
+    end
+  end
+  vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
+    group = 'suave.lua',
+    pattern = 'quickfix',
+    callback = function () _disable_local_qf_highlight() end
+  })
+  vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+    group = 'suave.lua',
+    pattern = '*', -- match against qf name.
+    callback = function () _disable_local_qf_highlight() end
+  })
+end
 ---------------------------------------------------------------------------------------------------
 function M.setup(opts)
   opts = opts or {}
@@ -70,25 +112,17 @@ function M.setup(opts)
     before_source = {},
     after_source = {},
   }
+
+  disable_local_qf_highlight()
 end
 
-local function _refresh_the_menu()
-  -- prepare items by browsing .suave/.
-  local items = {}
-  for dir in io.popen([[ find ]] .. get_project_suave_path() .. [[ -name '*.vim' ]]):lines() do
-    items[#items+1] = {
-      filename = vim.fn.fnamemodify(dir, ':t'),
-      lnum = tonumber(string.sub(io.popen([[ stat -f %Sm -t %Y%m%d%H%M ]] .. dir):read(), 3, 10)), -- timestamp
-      -- TODO: should maintain a mapping file to store users' note on each session.
-      text = '',
-    }
-  end
-  -- populate those items
-  vim.fn.setqflist({}, 'r', {
-    id = get_the_menu_id(),
-    items = items,
-  })
+
+function M.suave_folder_is_there()
+  local project_suave_path = get_project_suave_path()
+  local yes, _, code = os.rename(project_suave_path, project_suave_path)
+  return yes or (code == 13)
 end
+
 
 function M.toggle_menu()
   -- hint the user whether the current dir is suave root.
@@ -104,17 +138,18 @@ function M.toggle_menu()
 
   -- open a qflist window at the top.
   if the_menu_did_build() then
-    _refresh_the_menu()
+    refresh_the_menu()
     switch_to_the_menu()
     vim.cmd('top copen ' .. M.menu_height)
   end
 end
 
+
 function M.store_session(auto)
   if not M.suave_folder_is_there() then return end
 
   if not auto and not cursor_is_at_the_menu() then
-    print("Suave: Move your cursor to the menu to store session")
+    print("Suave: Please move your cursor to the menu window to store session!")
     return
   end
 
@@ -153,11 +188,12 @@ function M.store_session(auto)
   if not auto then M.toggle_menu() end
 end
 
+
 function M.restore_session(auto)
   if not M.suave_folder_is_there() then return end
 
   if not auto and not cursor_is_at_the_menu() then
-    print("Suave: Move your cursor to the menu to restore session")
+    print("Suave: Please move your cursor to the menu window to restore session!")
     return
   end
 
@@ -187,30 +223,6 @@ function M.restore_session(auto)
     end
   end
 end
-
----------------------------------------------------------------------------------------------------
-local function disable_local_qf_highlight()
-  local function _disable_local_qf_highlight()
-    if cursor_is_at_the_menu() then
-      vim.cmd([[
-        hi __SUAVE_QF_DISABLE guibg=NONE guifg=Directory
-        hi __SUAVE_NO_CURSORLINE guibg=NONE guifg=NONE
-      ]])
-      vim.cmd('set winhl=QuickFixLine:__SUAVE_QF_DISABLE,CursorLine:__SUAVE_NO_CURSORLINE')
-    end
-  end
-  vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
-    group = 'suave.lua',
-    pattern = 'quickfix',
-    callback = function () _disable_local_qf_highlight() end
-  })
-  vim.api.nvim_create_autocmd({ 'BufEnter' }, {
-    group = 'suave.lua',
-    pattern = '*', -- match against qf name.
-    callback = function () _disable_local_qf_highlight() end
-  })
-end
-disable_local_qf_highlight()
 
 
 return M

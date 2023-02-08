f1db83cd8b63be666f72a0cc0cbaabda2d8a81f5
nyngwang <nyngwang@gmail.com>
2023-02-09 02:32:50 +0800

    fix(json): shut-up `:messages` on `VimLeavePre` (Close #20)

--- lua/suave/utils/json.lua
+++ lua/suave/utils/json.lua
@@ -1,5 +1,6 @@
 local P = require('suave.utils.path')
 local M = {}
+M._on_VimLeave = false
 M.PROJECT_JSON_NAME = string.format('%s_storage', P.PROJECT_NAME)
 
 
@@ -38,47 +39,63 @@ end
 
 function M.read_from_project_json()
   if not P.folder_or_file_is_there() then
-    print("Suave: Failed to find the `.suave/` folder.")
+    if not M._on_VimLeave then
+      print("Suave: Failed to find the `.suave/` folder.")
+    end
     return { false, nil }
   end
 
   local fp = io.open(M.get_project_json_path(), 'r')
   if not fp then
-    print("Suave: Failed to read the project JSON.")
+    if not M._on_VimLeave then
+      print("Suave: Failed to read the project JSON.")
+    end
     return { false, nil }
   end
   local data = vim.json.decode(fp:read("*a"))
   fp:close()
-  print("Suave: Succeeded in reading the project JSON!")
+  if not M._on_VimLeave then
+    print("Suave: Succeeded in reading the project JSON!")
+  end
   return { true, data }
 end
 
 
 function M.write_to_project_json(data)
   if not P.folder_or_file_is_there() then
-    print("Suave: Failed to find the `.suave/` folder.")
+    if not M._on_VimLeave then
+      print("Suave: Failed to find the `.suave/` folder.")
+    end
     return false
   end
 
   if type(data) ~= 'table' then
-    print("Suave: The input of `write_to_project_json` should be a Lua table...")
+    if not M._on_VimLeave then
+      print("Suave: The input of `write_to_project_json` should be a Lua table...")
+    end
     return false
   end
   local fp = io.open(M.get_project_json_path(), 'w+')
   if not fp then
-    print("Suave: Failed to write to the project JSON...")
+    if not M._on_VimLeave then
+      print("Suave: Failed to write to the project JSON...")
+    end
     return false
   end
   fp:write(M.format_table(vim.json.encode(data)))
   fp:close()
-  print("Suave: Succeeded in writing to the project JSON!")
+  if not M._on_VimLeave then
+    print("Suave: Succeeded in writing to the project JSON!")
+  end
   return true
 end
 
 
 function M.get_or_create_project_file_data()
   if not P.folder_or_file_is_there() then
-    print("Suave: Failed to find the `.suave/` folder.")
+    if not M._on_VimLeave then
+      print("Suave: Failed to find the `.suave/` folder.")
+    end
     return { false, nil }
   end
 
@@ -86,7 +103,9 @@ function M.get_or_create_project_file_data()
   if not P.folder_or_file_is_there(M.get_project_json_path()) then
     vim.cmd(string.format('!touch %s', M.get_project_json_path()))
     M.write_to_project_json({})
-    print("Suave: A default project file has been created under the `.suave/` folder!")
+    if not M._on_VimLeave then
+      print("Suave: A default project file has been created under the `.suave/` folder!")
+    end
     return { true, {} }
   end
 
